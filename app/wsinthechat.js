8c158835c78315f6c3471d7c5b85e12db0eb593e
gapehorm <165452367+gapehorm@users.noreply.github.com>
2024-04-21 22:14:49 +0530

    Update wsinthechat.js

--- app/wsinthechat.js
+++ app/wsinthechat.js
@@ -1,3 +1,4 @@
-function InteractWithWLClick() {
-  window.location.href = 'https://www.youtube.com/watch?v=dQw4w9WgXcQ';
+export function InteractWithWLClick() {
+    // Function logic here
+    window.location.href = 'https://www.youtube.com/watch?v=dQw4w9WgXcQ';
 }
