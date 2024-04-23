70665b298646d8b94170d8a75b10b02d4357b792
gapehorm <165452367+gapehorm@users.noreply.github.com>
2024-04-23 22:25:52 +0530

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,7 @@ import Tabs from './Tabs'
 export default function Home() {
   return (
     <>
-      <button className='absolute scale-75 md:scale-100 md:top-5 md:right-10 md:left-[80%] right-[20%] left-[20%] top-5  border-[#BA4B00] text-[#BA4B00] border-[3px] px-4 py-2 rounded-full text-xl'>Connect Wallet</button>
+
       <Tabs />
     </>
   )
