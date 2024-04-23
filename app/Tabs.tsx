2b4e4b0b351ad6eda0dd8917aa7e9bdfe2a1f315
gapehorm <165452367+gapehorm@users.noreply.github.com>
2024-04-23 22:01:43 +0530

    Update Tabs.tsx

--- app/Tabs.tsx
+++ app/Tabs.tsx
@@ -28,12 +28,12 @@ const Tabs = () => {
   return (
     <main className="flex bg z-10 min-h-screen flex-col items-center justify-between">
       <div className='absolute min-w-[420px] min-h-[600px] scale-[0.6] md:scale-[1.15] bg-black/60 border-4 border-[#BA4B00] rounded-2xl p-4 px-14 top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2' style={{ marginTop: '30px' }}>
-        <div className='flex w-full items-center gap-5 justify-between'>
-          <button onClick={() => setState('Token Links')} className='flex-1 transition-all duration-300 hover:bg-[#A3531C] hover:text-white hover:border-white text-[#A3531C] border-[3px] text-xl font-bold border-[#BA4B00] rounded-3xl mb-5 py-2 '>Token Links</button>
-          <button onClick={() => setState('Tab 2')} className='flex-1 transition-all duration-300 hover:bg-[#A3531C] hover:text-white hover:border-white text-[#A3531C] border-[3px] text-xl font-bold border-[#BA4B00] rounded-3xl mb-5 py-2 '>WL Checker</button>
-        </div>
         {state === 'Token Links' ?
           <>
+            <div className='flex w-full items-center gap-5 justify-between'>
+              <button onClick={() => setState('Token Links')} className='flex-1 transition-all duration-300 hover:bg-[#A3531C] hover:text-white hover:border-white text-[#A3531C] border-[3px] text-xl font-bold border-[#BA4B00] rounded-3xl mb-5 py-2 '>Token Links</button>
+              <button onClick={() => setState('Tab 2')} className='flex-1 transition-all duration-300 hover:bg-[#A3531C] hover:text-white hover:border-white text-[#A3531C] border-[3px] text-xl font-bold border-[#BA4B00] rounded-3xl mb-5 py-2 '>WL Checker</button>
+            </div>
             <div className='flex flex-col justify-center items-center text-center'>
               <Image src={'https://images.guns.lol/KWEJy.png'} className='w-[80px] h-[80px]' width={11111} height={11111} alt='logo'></Image>
               <p className='bg-clip-text text-transparent bg-gradient-to-r from-[#F1740F] to-[#BA4B00] text-4xl font-extrabold'>Dragon Token</p>
@@ -49,27 +49,27 @@ const Tabs = () => {
             </div>
           </> : 
           <div className='flex flex-col justify-center items-center text-center'>
-            <p className='bg-clip-text text-transparent bg-gradient-to-r from-[#F1740F] to-[#BA4B00] text-4xl font-extrabold'>Heading</p>
             <div className='flex flex-row justify-center items-center text-center' style={{ marginTop: '20px' }}>
-               < <input
-    type="text"
-    placeholder="Enter address..."
-    value={address}
-    onChange={handleInputChange}
-    className="border-[#BA4B00] border-[3px] px-4 py-2 rounded-full text-xl outline-none focus:border-[#BA4B00] focus:ring-0"
-    style={{ marginBottom: '10px', width: '200px' }} // Changed marginRight to marginBottom
-  />
-  <button
-    className="border-[#BA4B00] text-[#BA4B00] border-[3px] px-4 py-2 rounded-full text-xl transition-all duration-300 hover:bg-[#BA4B00] hover:text-white"
-    onClick={handleButtonClick}
-  >
-    Check Whitelist
-  </button>
-  {whitelistLevelText && (
-    <p className="text-xl mt-4" style={{ color: 'white' }}>
-      {whitelistLevelText}
-    </p>
-  )}
+              <input
+                type="text"
+                placeholder="Enter address..."
+                value={address}
+                onChange={handleInputChange}
+                className="border-[#BA4B00] border-[3px] px-4 py-2 rounded-full text-xl outline-none focus:border-[#BA4B00] focus:ring-0"
+                style={{ marginBottom: '10px', width: '200px' }}
+              />
+              <button
+                className="border-[#BA4B00] text-[#BA4B00] border-[3px] px-4 py-2 rounded-full text-xl transition-all duration-300 hover:bg-[#BA4B00] hover:text-white"
+                onClick={handleButtonClick}
+              >
+                Check Whitelist
+              </button>
+            </div>
+            {whitelistLevelText && (
+              <p className="text-xl mt-4" style={{ color: 'white' }}>
+                {whitelistLevelText}
+              </p>
+            )}
           </div>
         }
       </div>
